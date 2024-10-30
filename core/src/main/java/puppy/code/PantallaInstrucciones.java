c97f9bcea03d67274250823d07fb05743a8a4171
Lucakuuuu <163463371+Lucakuuuu@users.noreply.github.com>
2024-10-30 13:09:56 -0300

    Implementación de la opción "aprender a jugar"

--- core/src/main/java/puppy/code/PantallaInstrucciones.java
+++ core/src/main/java/puppy/code/PantallaInstrucciones.java
@@ -8,9 +8,11 @@ import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class PantallaInstrucciones implements Screen {
@@ -19,35 +21,47 @@ public class PantallaInstrucciones implements Screen {
     private OrthographicCamera camera;
     private SpriteBatch batch;
     private Sound explosionSound;
-    private Texture imagenMovimiento;
     private Music gameMusic;
     private int score;
     private int ronda;
     private int velXAsteroides;
     private int velYAsteroides;
     private int cantAsteroides;
-    private boolean completado;
-    NaveSeleccionada naveSeleccionada;
+    private NaveSeleccionada naveSeleccionada;
+    private Texture imagenMovimiento;
+    private Texture imagenDisparo;
+    private Texture imagenHUD;
 
     private Nave4 nave;
     private ArrayList<Ball2> balls1 = new ArrayList<>();
     private ArrayList<Ball2> balls2 = new ArrayList<>();
     private ArrayList<Bullet> balas = new ArrayList<>();
 
+    private BitmapFont font;
+    private Texture pauseBackground;
+    private boolean isPaused;
+    private int seleccionActual = 0; // Mover seleccionActual a nivel de clase
+    private String[] opciones = {"Volver al juego", "Cambiar nave", "Salir"};
+
     public PantallaInstrucciones(SpaceNavigation game, NaveSeleccionada naveSeleccionada, int ronda, int score,
-                                 int velXAsteroides, int velYAsteroides, int cantAsteroides) {
+                         int velXAsteroides, int velYAsteroides, int cantAsteroides) {
         this.game = game;
         this.ronda = ronda;
         this.score = score;
         this.velXAsteroides = velXAsteroides;
         this.velYAsteroides = velYAsteroides;
         this.cantAsteroides = cantAsteroides;
-        this.completado = false;
         this.naveSeleccionada = naveSeleccionada;
 
         batch = game.getBatch();
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 1280, 720);
+
+        font = new BitmapFont();
+        font.getData().setScale(2f);
+        font.setColor(Color.WHITE);
+
+        pauseBackground = new Texture(Gdx.files.internal("pause_background.png"));
 
         // Cargar sonidos y música
         explosionSound = Gdx.audio.newSound(Gdx.files.internal("explosion.ogg"));
@@ -64,7 +78,6 @@ public class PantallaInstrucciones implements Screen {
             Gdx.audio.newSound(Gdx.files.internal(naveSeleccionada.soundShoot)),
             naveSeleccionada.speed);
         nave.setVidas(naveSeleccionada.vida);
-        System.out.println(naveSeleccionada.speed);
 
         // Crear asteroides dentro de los límites de la pantalla y con velocidades iniciales no nulas
         Random r = new Random();
@@ -80,39 +93,44 @@ public class PantallaInstrucciones implements Screen {
         }
     }
 
+    public void dibujaEncabezado() {
+        CharSequence str = "Vidas: " + nave.getVidas() + " Ronda: " + ronda;
+        game.getFont().getData().setScale(2f);
+        game.getFont().draw(batch, str, 10, 30);
+        game.getFont().draw(batch, "Score:" + this.score, Gdx.graphics.getWidth() - 130, 30);
+        game.getFont().draw(batch, "HighScore:" + game.getHighScore(), Gdx.graphics.getWidth() / 2 - 100, 30);
+    }
+
     public void mostrarControles() {
-        boolean bottomLEFTPressed = false;
-        boolean bottomRIGHTPressed = false;
-        boolean bottomUPPressed = false;
-        boolean bottomDOWNPressed = false;
-
         if (ronda == 1) {
-            // Verifica los controles solo en la primera ronda
-            if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) bottomLEFTPressed = true;
-            if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) bottomRIGHTPressed = true;
-            if(Gdx.input.isKeyPressed(Input.Keys.UP)) bottomUPPressed = true;
-            if(Gdx.input.isKeyPressed(Input.Keys.DOWN)) bottomDOWNPressed = true;
-
-            if(bottomLEFTPressed && bottomRIGHTPressed && bottomUPPressed && bottomDOWNPressed) {
-                completado = true;
-            } else {
-                batch.draw(imagenMovimiento, 250, 450, 700, 250);
-            }
-        } else {
-            completado = true; // Avanzar automáticamente en rondas posteriores
+            game.getBatch().draw(imagenMovimiento, 250, 450, 700, 250);
+        } else if (ronda == 2) {
+            game.getBatch().draw(imagenDisparo, 250, 450, 700, 250);
+        } else if (ronda == 3) {
+            game.getBatch().draw(imagenHUD, 250, 450, 700, 250);
         }
-    }/*
-            case 2:
-            case 3:
-            default:
-        }
-    }*/
+    }
 
     @Override
     public void render(float delta) {
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-        batch.begin();
 
+        if (Gdx.input.isKeyJustPressed(Input.Keys.ESCAPE)) {
+            isPaused = !isPaused;
+            if (isPaused) {
+                gameMusic.pause();
+            } else {
+                gameMusic.play();
+            }
+        }
+
+        if (isPaused) {
+            pause();
+            return;
+        }
+
+        batch.begin();
+        if(ronda == 3) dibujaEncabezado();
         mostrarControles();
 
         if (!nave.estaHerido()) {
@@ -181,13 +199,31 @@ public class PantallaInstrucciones implements Screen {
 
         batch.end();
 
-        // Nivel completado
-        if (completado) {
-            Screen ss = new PantallaInstrucciones(game, naveSeleccionada, ronda + 1, score,
+        if(ronda == 1 && Gdx.input.isKeyPressed(Input.Keys.ENTER)){
+            Screen ss = new PantallaInstrucciones(game, game.getNaveSeleccionada(), ronda + 1, score,
                 velXAsteroides + 3, velYAsteroides + 3, cantAsteroides + 1);
             ss.resize(1280, 720);
             game.setScreen(ss);
             dispose();
+        }
+        if(ronda == 2 && balls1.size() == 0 && Gdx.input.isKeyPressed(Input.Keys.ENTER)){
+            Screen ss = new PantallaInstrucciones(game, game.getNaveSeleccionada(), ronda + 1, score,
+                velXAsteroides + 0, velYAsteroides + 0, cantAsteroides + 4);
+            ss.resize(1280, 720);
+            game.setScreen(ss);
+            dispose();
+        }
+        if(ronda == 3 && balls1.size() == 0){
+            isPaused = !isPaused;
+            if (isPaused) {
+                gameMusic.pause();
+            } else {
+                gameMusic.play();
+            }
+            if (isPaused) {
+                pause();
+                return;
+            }
         }
     }
 
@@ -198,13 +234,73 @@ public class PantallaInstrucciones implements Screen {
     @Override
     public void show() {
         imagenMovimiento = new Texture(Gdx.files.internal("Movimiento.png"));
+        imagenDisparo = new Texture(Gdx.files.internal("Disparo.png"));
+        imagenHUD = new Texture(Gdx.files.internal("HUD.png"));
     }
 
     @Override
     public void resize(int width, int height) {}
 
     @Override
-    public void pause() {}
+    public void pause() {
+        if(ronda == 3){
+            batch.begin();
+            game.getBatch().draw(pauseBackground, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+            game.getFont().draw(game.getBatch(), "> Volver al menú", Gdx.graphics.getWidth() / 2 - 100, Gdx.graphics.getHeight() / 2 - 100);
+            font.draw(batch, "FELICIDADES\nTUTORIAL COMPLETADO", Gdx.graphics.getWidth() / 2- 100, Gdx.graphics.getHeight() / 2);
+            batch.end();
+            if( Gdx.input.isKeyPressed(Input.Keys.ENTER)){
+                Screen ss = new PantallaMenu(game);
+                ss.resize(1280, 720);
+                game.setScreen(ss);
+                dispose();
+            }
+        }
+        else {
+            batch.begin();
+            batch.draw(pauseBackground, 0, 0, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+
+            // Dibujar las opciones del menú
+            for (int i = 0; i < opciones.length; i++) {
+                if (i == seleccionActual) {
+                    // Resaltar la opción seleccionada
+                    game.getFont().draw(game.getBatch(), "> " + opciones[i], 500, 400 - i * 50);
+                } else {
+                    game.getFont().draw(game.getBatch(), opciones[i], 500, 400 - i * 50);
+                }
+            }
+
+            // Navegación de opciones con las teclas de flecha
+            if (Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
+                seleccionActual = (seleccionActual - 1 + opciones.length) % opciones.length;
+            } else if (Gdx.input.isKeyJustPressed(Input.Keys.DOWN)) {
+                seleccionActual = (seleccionActual + 1) % opciones.length;
+            }
+
+            // Seleccionar opción con la tecla Enter
+            if (Gdx.input.isKeyJustPressed(Input.Keys.ENTER)) {
+                switch (seleccionActual) {
+                    case 0: // "Volver al juego"
+                        isPaused = false;
+                        gameMusic.play();
+                        break;
+                    case 1: // "Cambiar nave"
+                        // Implementar la lógica para cambiar nave
+                        break;
+                    case 2: // "Salir"
+                        Screen ss = new PantallaMenu(game);
+                        ss.resize(1280, 720);
+                        game.setScreen(ss);
+                        dispose();
+                        break;
+                }
+            }
+
+            // Dibuja el encabezado "PAUSA"
+            font.draw(batch, "PAUSA", Gdx.graphics.getWidth() / 2 - 50, Gdx.graphics.getHeight() - 100);
+            batch.end();
+        }
+    }
 
     @Override
     public void resume() {}
@@ -216,5 +312,14 @@ public class PantallaInstrucciones implements Screen {
     public void dispose() {
         explosionSound.dispose();
         gameMusic.dispose();
+        /* Liberación adicional de recursos de texturas y sonidos de la nave y asteroides
+        nave.dispose();
+        for (Ball2 ball : balls1) {
+            ball.dispose();
+        }
+        for (Bullet bala : balas) {
+            bala.dispose();
+        }
+         */
     }
 }
